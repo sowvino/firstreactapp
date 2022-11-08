d412cac0a9590b5995d7578954b5fdb1c17b7359
sowvino <106254533+sowvino@users.noreply.github.com>
2022-11-08 18:43:41 +0100

    hello file added

---
+++ src/Hello.js
@@ -0,0 +1,12 @@
+//import logo from './logo.svg';
+import './Hello.css';
+import React, { component } from 'react';
+
+class Hello extends component {
+    render() {
+        return (
+            <h1>HelloWorld!</h1>
+        )
+    }
+}
+export default Hello;
