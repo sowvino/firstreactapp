d6b2d0e7c4ebe8063fba6f8bd2d94f4418b8e086
sowvino <106254533+sowvino@users.noreply.github.com>
2022-11-08 18:54:40 +0100

    modified

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hello from './Hello.js';
+import Hello from './Hello';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
